5610a89279ba273953c996c6d4fcab29604833a1
Roman Mingazeev <Direnol@yandex.ru>
2018-10-04 08:45:24 +0700

    Initial commit

---
+++ lib/valera.ex
@@ -0,0 +1,18 @@
+defmodule Valera do
+  @moduledoc """
+  Documentation for Valera.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Valera.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/valera_test.exs
@@ -0,0 +1,8 @@
+defmodule ValeraTest do
+  use ExUnit.Case
+  doctest Valera
+
+  test "greets the world" do
+    assert Valera.hello() == :world
+  end
+end
