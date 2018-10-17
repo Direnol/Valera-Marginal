58832f29864c7e7f4b1042b1dfda818d542dedd8
evg-kazartseff <evg.kazartseff@yandex.ru>
2018-10-17 21:49:34 +0700

    Обновил model

--- lib/model.rb
+++ lib/model.rb
@@ -13,18 +13,40 @@ module Model
   end
 
   class Action
+    def self.descendants
+      ObjectSpace.each_object(Class).select { |child| child < self }
+    end
+    def run(human_state)
+      human_state
+    end
+  end
+
+  class GoToWork < Action
+    def run(human_state)
+      human_state
+    end
+  end
+
+  class AvailableActions
+    attr_accessor :actions
+    def initialize
+      @actions
+    end
   end
 
   class Human
-    attr_reader :state
+    attr_reader :state, :available_actions
 
     def initialize
-      state = HumanState.new
+      @state = HumanState.new
+      @available_actions = AvailableActions.new
     end
-
-    def get_list_of_action; end
   end
 
   class Valera < Human
+
   end
+  valera = Valera.new
+  p valera.state.health.to_s
 end
+
