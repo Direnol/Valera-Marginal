dad1db6939ff9a2bfb7ff6e80c22349cd55907a2
Evgenii Kazarcev <evg.kazartseff@yandex.ru>
2018-10-17 20:27:16 +0700

    Начало модели

--- app/model.rb
+++ app/model.rb
@@ -1,5 +1,34 @@
 module Model
-  class Model
+  class HumanState
+    attr_accessor :health, :mana, :cheerfulness, :fatigue, :money
+  end
+
+  class Action
+
+  end
+
+  class Human
+    attr_reader :state
+
+    def init
+      state = HumanState.new
+    end
+
+    def get_list_of_action
+
+    end
+
+  end
+
+  class Valera < Human
 
   end
 end
+
+human_state = HumanState.new
+
+human_state.health = 100
+human_state.mana = 0
+
+valera = Valera.new
+
