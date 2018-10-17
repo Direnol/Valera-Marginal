039fadb7694202f986cadf16a13317fd95ad50da
Roman Mingazeev <direnol@yandex.ru>
2018-10-17 20:29:26 +0700

    Инициализация MVC

--- app/controller.rb
+++ app/controller.rb
@@ -1,3 +1,24 @@
 module Controller
+  load './app/viewer.rb'
+  load './app/model.rb'
 
+  class Controller
+    def save_state(path); end
+
+    def load_state(path); end
+
+    def execute(command); end
+
+    def run
+      loop do
+        command = gets.chomp
+        valera = Model::Model.new
+        break if (command == 'exit') || (command == 'q')
+
+        Viewer::Viewer.print valera.to_s
+        act = Viewer::Viewer.ask
+
+      end
+    end
+  end
 end
